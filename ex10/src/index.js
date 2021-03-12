bb4478a03bb1c9f63d36db1ebafacb209b0b2fbe
SplaY <mirza_messic@hotmail.com>
2021-03-12 09:43:08 +0100

    Added tenth index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+
+    // Only change code below this line
+    var myFirstName = "Mirza"
+    var myLastName = "Mesic"
+    var myHomeTown = "Travnik"
+    // Only change code abowe this line
+
+    return {
+       myFirstName,
+       myLastName,
+       myHomeTown
+    };
+}
+
+console.log(main());
+
+
+module.export = main;
