d63ab58263d2cf13cf886a9c2a4dbc8a8c4ff05f
SplaY <mirza_messic@hotmail.com>
2021-03-12 09:17:56 +0100

    Added fourth index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+    // Only change code below this line
+    
+    var myDecimal = 20.2;
+
+    var myDecimalE = 254e-5;
+    
+    // Only change code abowe this line
+     return {
+         myDecimal,
+         myDecimalE
+     };
+    }
+     console.log(main());
+    
+     module.export = main
